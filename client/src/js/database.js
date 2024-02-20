86acb0158280d50db671d1d0e181f54c3f8d4a2d
monsosam <monsonsamuel5@gmail.com>
2024-02-19 17:45:20 -0700

    ffgf

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,24 +17,24 @@ export const putDb = async (content) => {
   console.log('PUT to the database');
 
   
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readwrite');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, todo: content });
-  const result = await request;
+  const result = await store.put({ id: 1, value: content });
   console.log('Data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readonly');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
-  const result = await request;
-  console.log('Data retrieved from the database', result);
-  return result;
+
+  const result = await store.getAll();
+  console.log('result.value', result);
+  return result?.value;
 };
 
 initdb();
